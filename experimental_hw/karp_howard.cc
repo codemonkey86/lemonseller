48df928bfb6a40b65b276ba4b5d563bbea2ca2d9
Ben <syrvantus@gmail.com>
2013-05-05 12:10:17 -0400

    Fixed bugs with adding weights and creating the initial line graph

--- experimental_hw/karp_howard.cc
+++ experimental_hw/karp_howard.cc
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <algorithm>
-#include <vector>      
+#include <vector>     
 #include <lemon/static_graph.h>
 #include <lemon/karp_mmc.h>
 #include <lemon/howard_mmc.h>
@@ -16,44 +16,64 @@
 using namespace lemon;
 using namespace std;
 
-int main (){
+int main () {
     bool DEBUG = false;
+    bool PATH_INFO = false;
+
     std::vector<std::pair<int,int> > temp_vector;
     std::vector<std::pair<int,int> > arcs;
     srand (time(NULL));
     //int numarcs = (rand() % 10) + 16;     // range 16-26
-    //int numvertices = (rand() % 5) + 8;   // range 8-13 
+    //int numvertices = (rand() % 5) + 8;   // range 8-13
     int numarcs = 75;
+    int real_numarcs = 0;
     int numvertices = 100;
-    cout << "Building Graph" << endl;
-    cout << "Number of arcs: " << numarcs << endl;
+   
+    cout << "Number of extra arcs: " << numarcs << endl;
     cout << "Number of vertices: " << numvertices << endl;
+   
+    // Start with line from 0 -> 1 -> ... -> (numvertices - 1) 
+    // this insures the graph is connected
+    if (DEBUG) cout << "Building line graph" << endl;
+    for (int i = 0; i < numvertices - 1; i++) {
+        temp_vector.push_back(std::make_pair(i, i+1));
+    }
+
+    // Adds arcs of random source and target
+    if (DEBUG) cout << "Adding extra arcs" << endl;
     for (int i = 0; i < numarcs; i++) {
         temp_vector.push_back(std::make_pair(rand() % numvertices, rand() % numvertices));
-        if ( i + 1 < numvertices) {
-            temp_vector.push_back(std::make_pair(i, i+1));
-        }
     }
+
     // Removing duplicates and self-loops
+    if (DEBUG) cout << "Sorting arcs" << endl;
     std::sort(temp_vector.begin(), temp_vector.end());
+    if (DEBUG) cout << "Removing duplicate arcs" << endl;
     temp_vector.erase(unique(temp_vector.begin(), temp_vector.end()), temp_vector.end());
+    if (DEBUG) cout << "Removing self-loops" << endl;
+
     for (std::vector< std::pair<int,int> >::iterator it=temp_vector.begin(); it!=temp_vector.end(); ++it){
         if (it->first != it->second) {
             arcs.push_back(std::make_pair(it->first, it->second));
+            real_numarcs++;
         }
     }
-    // Prints elements if DEBUG is set
     if (DEBUG) {
+        cout << "List of arcs" << endl;
         for (std::vector< std::pair<int,int> >::iterator it=arcs.begin(); it!=arcs.end(); ++it){
             cout << "(" << it->first << "," << it->second << ")" << endl;
         }
+        cout << "Number of arcs: " << real_numarcs << endl;
+
     }
-    else {
+   
     StaticDigraph gr;
     gr.build(numvertices, arcs.begin(), arcs.end());
     StaticDigraph::ArcMap<int> map(gr);
-   
-    for (int i = 0; i < numarcs; i++) {
+
+    // Adding random weights
+    if (DEBUG) cout << "Adding weights to arcs" << endl;
+    for (int i = 0; i < real_numarcs; i++) {
         map[gr.arc(i)] = (rand() % 10) + 1;
     }
    
@@ -62,25 +82,26 @@ int main (){
 
     cout << endl;
     if (karp_algo.run() && howard_algo.run()){
-        cout << "Number of arcs on cycle" << endl;
-        cout << "Karp:\t" << karp_algo.cycleSize() << endl;
-        cout << "Howard:\t" << howard_algo.cycleSize() << endl;
-        cout << "Cost of cycle" << endl;
-        cout << "Karp:\t" << karp_algo.cycleCost() << endl;
-        cout << "Howard:\t" << howard_algo.cycleCost() << endl;
-        cout << "Minimum Cycle Mean" << endl;
-        cout << "Karp:\t" << karp_algo.cycleMean() << endl;
-        cout << "Howard:\t" << howard_algo.cycleMean() << endl;
-        //Path<StaticDigraph> p = karp_algo.cycle();
-        //concepts::Path<StaticDigraph>::Arc a(karp_algo.cycle().nth(0));
-        //THIS is verification that we get a path may not belong in final code,  also, ARCIT may be preferrable, but I had trouble getting it to work properly
-        //for (int i=0;i<p.length(); i++){
-        //    cout<< endl << "ARC from " << gr.id(gr.source(howard_algo.cycle().nth(i))) << " to " << gr.id(gr.target(howard_algo.cycle().nth(i)));
-        //}
-        cout << endl;
+        cout << "\tCost -- Arcs -- MMC" << endl;
+        cout << "Karp:\t" << karp_algo.cycleCost() << "\t" << karp_algo.cycleSize() << "\t" << karp_algo.cycleMean() << endl;
+        cout << "Howard:\t" << howard_algo.cycleCost() << "\t" << howard_algo.cycleSize() << "\t" << howard_algo.cycleMean() << endl;
+        if (PATH_INFO) {
+            Path<StaticDigraph> karp_path = karp_algo.cycle();
+            Path<StaticDigraph> howard_path = howard_algo.cycle();
+           
+            cout << endl << "Karp path:";
+            for (int i=0; i < karp_path.length(); i++){
+                cout << endl << i + 1 << "-ARC from " << gr.id(gr.source(karp_path.nth(i))) << " to " << gr.id(gr.target(karp_path.nth(i)));
+            }
+            cout << endl << "Howard path:";
+            for (int i=0; i < karp_path.length(); i++){
+                cout<< endl << i + 1 << "-ARC from " << gr.id(gr.source(howard_path.nth(i))) << " to " << gr.id(gr.target(howard_path.nth(i)));
+
+            }
+            cout << endl;
+        }
     } else {
         cout << "Cycle free - Please retry!" << endl;
-    }
     }
     //TODO maybe compare with ListGraph, explore tolerancce
 }
